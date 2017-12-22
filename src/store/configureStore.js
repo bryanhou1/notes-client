2da4fda81bbec60c1292374e4b29c3919f4e6c11
Bryan Hou <bryan.hou94@gmail.com>
2017-12-22 17:48:52 -0500

    add thunk middleware for fetch purposes later and redux devtools extension for debugging

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers/index';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer,  composeEnhancers(applyMiddleware(thunk)
+    ));
 
 
-export default createStore(rootReducer);
+
+export default store;
