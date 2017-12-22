a16ded0533e839db156a7420c3dfbc0f4f771a0a
Bryan Hou <bryan.hou94@gmail.com>
2017-12-22 17:23:31 -0500

    add react-redux package and wrap app with provider store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
 import App from './components/App';
+import store from './store/configureStore.js'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+	, document.getElementById('root'));
 registerServiceWorker();
