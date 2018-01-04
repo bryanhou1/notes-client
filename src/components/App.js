a6252e0393d460e4d59949accbafa5a311a0b0d2
Bryan Hou <bryan.hou94@gmail.com>
2018-01-04 11:49:51 -0500

    link up signin and signupform component to routes

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import NavBar from './NavBar';
+import SignInForm from './SignInForm';
+import SignUpForm from './SignUpForm';
 
 class App extends Component {
   render() {
@@ -11,8 +13,8 @@ class App extends Component {
           <NavBar />
           <div id="main-container">
             <Route exact path="/" render={() => <h3>Welcome!</h3>} />
-            <Route exact path="/sign_in" render={() => <h3>Sign In</h3>} />
-            <Route exact path="/sign_up" render={() => <h3>Sign up</h3>} />
+            <Route exact path="/sign_in" component={SignInForm} />
+            <Route exact path="/sign_up" component={SignUpForm} />
           </div>
         </div>
       </Router>
