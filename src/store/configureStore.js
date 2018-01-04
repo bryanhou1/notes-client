1605690d1551b3bdb8b94f8460c8ac1c2d57f7f6
Bryan Hou <bryan.hou94@gmail.com>
2018-01-04 11:51:18 -0500

    fix store with proper thunkmiddleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers/index';
-import thunk from 'redux-thunk';
+import thunkMiddleware from 'redux-thunk';
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const initialStore = {};
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer,  composeEnhancers(applyMiddleware(thunk)
-    ));
-
-
+const store = createStore(rootReducer, initialStore, composeEnhancers(
+	applyMiddleware(thunkMiddleware)
+))
 
 export default store;
