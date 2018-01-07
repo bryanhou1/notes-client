897600bc3ab1fe80d18c774ff28e93e3f4104f32
Bryan Hou <bryan.hou94@gmail.com>
2018-01-06 20:47:23 -0500

    modify Messages component accordingly

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -2,22 +2,34 @@ import React, {Component} from 'react';
 
 class Messages extends Component {
 
+  arrayToList() {
+    const {text} = this.props.messages;
+
+    return (
+      <ul>
+        {text.map((str) => <li>{str}</li>)}
+      </ul>
+    )
+  }
 
   render() {
     const {text, style} = this.props.messages;
 
   	if (text) {
+
       if (style === "success") {
         return (
           <div className="alert alert-success">
-            {text}
+            <ul>
+              {this.arrayToList()}
+            </ul>
           </div>
         )
       }
       if (style === "error") {
         return (
           <div className="alert alert-danger">
-            {text}
+            {this.arrayToList()}
           </div>
         )
       }
