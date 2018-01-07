bcb32cede44800849d989dff76adee9b59e8660c
Bryan Hou <bryan.hou94@gmail.com>
2018-01-07 01:30:25 -0500

    move cdn asset to proper location

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,27 +3,30 @@ import {Navbar, Nav, NavItem} from 'react-bootstrap';
 import {LinkContainer} from 'react-router-bootstrap';
 import Logo from '../logo.svg';
 
-const NavBar = () => 
-  <div>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
-    <Navbar className="not_signed_in">
-      <Navbar.Header>
-        <Navbar.Brand>
-          <LinkContainer to="/">
-            <img src={Logo} alt="Logo" href="#" />
+const NavBar = () => {
+  return (
+    <div>
+      <Navbar className="not_signed_in">
+        <Navbar.Header>
+          <Navbar.Brand>
+            <LinkContainer to="/">
+              <img src={Logo} alt="Logo" href="#" />
+            </LinkContainer>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav pullRight>
+          <LinkContainer to="/sign_up">
+            <NavItem>Sign Up</NavItem>
           </LinkContainer>
-        </Navbar.Brand>
-      </Navbar.Header>
-      <Nav pullRight>
-        <LinkContainer to="/sign_up">
-          <NavItem>Sign Up</NavItem>
-        </LinkContainer>
-        <LinkContainer to="/sign_in">
-          <NavItem>Sign In</NavItem>
-        </LinkContainer>
-      </Nav>
-    </Navbar>
-  </div>;
+          <LinkContainer to="/sign_in">
+            <NavItem>Sign In</NavItem>
+          </LinkContainer>
+        </Nav>
+      </Navbar>
+    </div>
+  )
+}
+
 
 
 
