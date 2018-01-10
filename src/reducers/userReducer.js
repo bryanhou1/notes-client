26e0bca8c14d194fe0afc3e3a5018382da0db5c4
Bryan Hou <bryan.hou94@gmail.com>
2018-01-09 21:51:13 -0500

    fix syntactical error in User Reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -4,15 +4,15 @@ const initialState = {user: {jwt: ""}, isLoggingIn: false}
 export default function userReducer(state = initialState, action){  
   switch (action.type) {
     case "MATCH_LOCAL_STORAGE_TO_STATE":
-      return {...state, user: {jwt: localStorage.token})
+      return {...state, user: {jwt: localStorage.token}}
     case "FETCH_CURRENT_USER_SUCCESS":
-      return {...state, user: {...state.user ,...action.user})
+      return {...state, user: {...state.user ,...action.user}}
     case "LOGIN_START":
     	return {...state, isLoggingIn: true}
     case "LOGIN_FAILURE":
     	return {...state, isLoggingIn: false}
     case "LOGIN_SUCCESS":
-    	return {...state, user: action.user, isLoggingIn: false)
+    	return {...state, user: action.user, isLoggingIn: false}
     case "LOGOUT":
       return initialState;
     default:
